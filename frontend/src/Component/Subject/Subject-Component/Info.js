8fc9d265b67335f12d640344a502e70e6407db98
Monzzzz <elmond.pattanan@gmail.com>
2023-11-12 00:23:22 -0800

    subject-page

--- frontend/src/Component/Subject/Subject-Component/Info.js
+++ frontend/src/Component/Subject/Subject-Component/Info.js
@@ -20,11 +20,28 @@ export default function Info() {
         <div className="col-lg-4 d-flex justify-content-center">
           <div className="today-container col-11 d-flex flex-column">
             <h2 className="mb-3">Today</h2>
-            <div className="quiz-container">
+            <div className="quiz-container mb-5">
               <h5>Quiz</h5>
               {/*map list of the quiz */}
               <li>Algebra</li>
               <li>Geometry</li>
+            </div>
+            <div className="flashcard-container">
+              <h4 className="mb-3">Flash Card</h4>
+              <div className="flashcard-set-container">
+                <div className="each-flashcard m-2">
+                  <h5>Math</h5>
+                </div>
+                <div className="each-flashcard m-2">
+                  <h5>Computer Science</h5>
+                </div>
+                <div className="each-flashcard m-2">
+                  <h5>Social Studies</h5>
+                </div>
+                <div className="each-flashcard m-2">
+                  <h5>Physics</h5>
+                </div>
+              </div>
             </div>
           </div>
           <div className="vertical-line-subject text-end" />
